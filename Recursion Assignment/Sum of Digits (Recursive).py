6e6ba1cf43c3a1f7b46fee53c7a8c2f0f0aa0088
SUMAN SHEKHAR <60771780+ss4621-dev@users.noreply.github.com>
2021-05-24 20:18:14 +0530

    Create Sum of Digits (Recursive).py

---
+++ Recursion Assignment/Sum of Digits (Recursive).py
@@ -0,0 +1,11 @@
+## Read input as specified in the question.
+## Print output as specified in the question.
+def sum(n):
+    if n == 0:
+        return 0
+    else:
+        return n % 10 + sum(n//10)
+
+
+n = int(input())
+print(sum(n))
