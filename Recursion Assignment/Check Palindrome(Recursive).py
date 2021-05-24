0fee98d8480d834dce05f78a6056846cc74e156e
SUMAN SHEKHAR <60771780+ss4621-dev@users.noreply.github.com>
2021-05-24 20:17:35 +0530

    Create Check Palindrome(Recursive).py

---
+++ Recursion Assignment/Check Palindrome(Recursive).py
@@ -0,0 +1,19 @@
+## Read input as specified in the question.
+## Print output as specified in the question.
+# Read input as specified in the question.
+# Print output as specified in the question.
+def is_palindrome(s):
+    if len(s) < 1:
+        return True
+    else:
+        if s[0] == s[-1]:
+            return is_palindrome(s[1:-1])
+        else:
+            return False
+
+
+a = str(input())
+if(is_palindrome(a) == True):
+    print("true")
+else:
+    print("false")
