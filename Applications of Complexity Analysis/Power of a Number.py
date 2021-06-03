2adf7294fa4bc998376e683949c062d98d48ab6b
SUMAN SHEKHAR <60771780+ss4621-dev@users.noreply.github.com>
2021-06-03 11:01:36 +0530

    Create Power of a Number.py

---
+++ Applications of Complexity Analysis/Power of a Number.py
@@ -0,0 +1,10 @@
+def power(x, n):
+    if n == 0:
+        return 1
+    return x*power(x, n- 1)
+
+# Main
+from sys import setrecursionlimit
+setrecursionlimit(11000)
+x, n=list(int(i) for i in input().strip().split(' '))
+print(power(x, n))
