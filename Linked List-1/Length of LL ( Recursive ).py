f52c1d1943a8aa3ce4844c2afe7f31cb202d840e
SUMAN SHEKHAR <60771780+ss4621-dev@users.noreply.github.com>
2021-06-21 09:41:42 +0530

    Create Length of LL ( Recursive ).py

---
+++ Linked List-1/Length of LL ( Recursive ).py
@@ -0,0 +1,55 @@
+from sys import stdin , setrecursionlimit
+setrecursionlimit(10**6)
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# Taking Input Using Fast I/O
+def takeInput() :
+    head = None
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None :
+            head = newNode
+            tail = newNode
+
+        else :
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+    return head
+
+
+# To print the linked list 
+def printLinkedList(head) :
+
+    while head is not None :
+        print(head.data, end = " ")
+        head = head.next
+
+    print()
+
+def lengthRecursive(head):
+    if head is None:
+        return 0
+    return 1 + lengthRecursive(head.next)
+    
+
+# Main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+    head = takeInput()
+    ans=lengthRecursive(head)
+    print(ans)
+    t -= 1 
