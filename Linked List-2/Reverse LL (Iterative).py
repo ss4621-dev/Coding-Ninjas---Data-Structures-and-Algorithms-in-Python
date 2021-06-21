6bfd927258d51cef8d6bfc520301cef12df3c993
SUMAN SHEKHAR <60771780+ss4621-dev@users.noreply.github.com>
2021-06-21 09:48:14 +0530

    Create Reverse LL (Iterative).py

---
+++ Linked List-2/Reverse LL (Iterative).py
@@ -0,0 +1,84 @@
+from sys import stdin , setrecursionlimit
+setrecursionlimit(10**6)
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# Taking Input Using Fast I/O
+def takeInput() :
+    head = None
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None :
+            head = newNode
+            tail = newNode
+
+        else :
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+
+    return head
+
+
+# To print the linked list 
+def printLinkedList(head) :
+
+    while head is not None :
+        print(head.data, end = " ")
+        head = head.next
+
+    print()
+
+
+def reverse(head):
+    before = None
+    current = head
+    if current is None:
+        return
+    after = current.next
+    while after:
+        current.next = before
+        before = current
+        current = after
+        after = after.next
+    current.next = before
+    head = current
+    
+    return head
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+
+    head = takeInput()
+    
+    ans=reverse(head)
+    printLinkedList(ans)
+
+    t -= 1 
+
