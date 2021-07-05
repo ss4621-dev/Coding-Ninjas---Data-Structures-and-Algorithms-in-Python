44818e27cba6a3c3b8995a6c7d397f75431c765b
SUMAN SHEKHAR <60771780+ss4621-dev@users.noreply.github.com>
2021-07-05 10:28:20 +0530

    Update Queue Using LL.py

--- Queues/Queue Using LL.py
+++ Queues/Queue Using LL.py
@@ -1,31 +1,109 @@
 
 from sys import stdin
 
-def countBracketReversals(inputString) :
-    # Your code goes here
-    if(len(inputString) == 0):
-        return 0
-    if(len(inputString) % 2 != 0):
-        return -1
+
+#Following is the structure of the node class for a Singly Linked List
+class Node :
+
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+
+
+class Queue :
+
+    #Define data members and __init__()
+    def __init__(self):
+        self.__LL = []
+        self.__count = 0
+        self.__head = None
+        self.__tail = None
+        
     
-    s = []
-    for ele in inputString:
-        if ele == '{':
-            s.append(ele)
+
+
+
+    '''----------------- Public Functions of Stack -----------------'''
+
+    def getSize(self) :
+        #Implement the getSize() function
+        return self.__count
+
+
+
+    def isEmpty(self) :
+        #Implement the isEmpty() function
+        return self.__count == 0
+
+
+
+    def enqueue(self, data) :
+        #Implement the enqueue(element) function
+        if self.__head is None:
+            newNode = Node(data)
+            self.__head = newNode
+            self.__tail = newNode
+            self.__count += 1
+        
         else:
-            if(len(s) > 0 and s[-1] == '{'):
-                s.pop()
-            else:
-                s.append(ele)
-    count = 0
-    while(len(s) != 0):
-        c1 = s.pop()
-        c2 = s.pop()
-        if c1 != c2:
-            count += 2
-        else:
-            count += 1
-    return count
-    
+            newNode = Node(data)
+            self.__tail.next = newNode
+            self.__count += 1
+            self.__tail = self.__tail.next
+            
+
+
+
+    def dequeue(self) :
+        #Implement the dequeue() function
+        if self.__count == 0:
+            return -1
+        
+        element = self.__head.data
+        self.__head = self.__head.next
+        self.__count -= 1
+        return element
+
+
+
+    def front(self) :
+        #Implement the front() function
+        if self.__count == 0:
+            return -1
+        
+        return self.__head.data
+        
+
+
+
+
 #main
-print(countBracketReversals(stdin.readline().strip()))
+q = int(stdin.readline().strip())
+
+queue = Queue()
+
+while q > 0 :
+
+    inputs = stdin.readline().strip().split(" ")
+    choice = int(inputs[0])
+
+    if choice == 1 :
+        data = int(inputs[1])
+        queue.enqueue(data)
+
+    elif choice == 2 :
+        print(queue.dequeue())
+
+    elif choice == 3 :
+        print(queue.front())
+
+    elif choice == 4 : 
+        print(queue.getSize())
+
+    else :
+        if queue.isEmpty() :
+            print("true")
+        else :
+            print("false")
+
+    q -= 1
