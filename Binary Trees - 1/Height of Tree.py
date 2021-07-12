3d10c507875f304b6506540ccbef3bf9f055969e
SUMAN SHEKHAR <60771780+ss4621-dev@users.noreply.github.com>
2021-07-12 21:01:13 +0530

    Create Height of Tree.py

---
+++ Binary Trees - 1/Height of Tree.py
@@ -0,0 +1,66 @@
+from sys import stdin, setrecursionlimit
+import queue
+
+setrecursionlimit(10 ** 6)
+
+
+#Following is the structure used to represent the Binary Tree Node
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def height(root) :
+	#Your code goes here
+    if root == None:
+        return 0
+    leftHeight = height(root.left)
+    rightHeight = height(root.right)
+    if leftHeight > rightHeight:
+        return(1+leftHeight)
+    return(1+rightHeight)
+
+
+#Taking level-order input using fast I/O method
+def takeInput():
+    levelOrder = list(map(int, stdin.readline().strip().split(" ")))
+    start = 0
+    
+    length = len(levelOrder)
+
+    if length == 1 :
+        return None
+    
+    root = BinaryTreeNode(levelOrder[start])
+    start += 1
+
+    q = queue.Queue()
+    q.put(root)
+
+    while not q.empty():
+        currentNode = q.get()
+
+        leftChild = levelOrder[start]
+        start += 1
+
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+
+        rightChild = levelOrder[start]
+        start += 1
+
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+
+    return root
+
+# Main
+root = takeInput()
+
+h = height(root)
+print(h)
