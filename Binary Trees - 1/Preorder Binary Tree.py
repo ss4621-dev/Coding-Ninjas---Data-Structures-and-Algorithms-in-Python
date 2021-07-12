e4a54962a4c52b0b5888c7ed1ebc96aa724c9d06
SUMAN SHEKHAR <60771780+ss4621-dev@users.noreply.github.com>
2021-07-12 20:59:09 +0530

    Create Preorder Binary Tree.py

---
+++ Binary Trees - 1/Preorder Binary Tree.py
@@ -0,0 +1,63 @@
+from sys import stdin, setrecursionlimit
+import queue
+
+setrecursionlimit(10 ** 6)
+
+#Following the structure used for Binary Tree
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+
+def preOrder(root):
+	#Your code goes here
+    if root == None:
+        return
+    if root != None:
+        print(root.data, end=' ')
+    preOrder(root.left)
+    preOrder(root.right)
+    
+
+
+
+#Taking level-order input using fast I/O method
+def takeInput():
+    levelOrder = list(map(int, stdin.readline().strip().split(" ")))
+    start = 0
+
+    length = len(levelOrder)
+
+    root = BinaryTreeNode(levelOrder[start])
+    start += 1
+
+    q = queue.Queue()
+    q.put(root)
+
+    while not q.empty():
+        currentNode = q.get()
+
+        leftChild = levelOrder[start]
+        start += 1
+
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+
+        rightChild = levelOrder[start]
+        start += 1
+
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+
+    return root
+
+# Main
+root = takeInput()
+preOrder(root)
