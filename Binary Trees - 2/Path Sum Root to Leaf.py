7da90fec4d9116c4399155ea6cb71bfc2a7e5613
SUMAN SHEKHAR <60771780+ss4621-dev@users.noreply.github.com>
2021-07-12 21:09:49 +0530

    Create Path Sum Root to Leaf.py

---
+++ Binary Trees - 2/Path Sum Root to Leaf.py
@@ -0,0 +1,130 @@
+from sys import stdin, setrecursionlimit
+import queue
+
+setrecursionlimit(10 ** 6)
+
+
+#Following is the structure used to represent the Binary Tree Node
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+
+def rootToLeafPathsSumToK(root, k, string):
+	#Your code goes here
+    if root is None:
+        return
+    if root.left is None and root.right is None:
+        if root.data == k:
+            string.append(root.data)
+            print(*string)
+            string.pop()
+            return
+    string.append(root.data)
+    rootToLeafPathsSumToK(root.left, k-root.data, string)
+    rootToLeafPathsSumToK(root.right, k-root.data, string)
+    string.pop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Taking level-order input using fast I/O method
+def takeInput():
+    levelOrder = list(map(int, stdin.readline().strip().split(" ")))
+    start = 0
+    
+    length = len(levelOrder)
+
+    if length == 1 :
+        return None
+    
+    root = BinaryTreeNode(levelOrder[start])
+    start += 1
+
+    q = queue.Queue()
+    q.put(root)
+
+    while not q.empty():
+        currentNode = q.get()
+
+        leftChild = levelOrder[start]
+        start += 1
+
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+
+        rightChild = levelOrder[start]
+        start += 1
+
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+
+    return root
+
+    
+def printLevelWise(root):
+    if root is None:
+        return
+
+    inputQ = queue.Queue()
+    outputQ = queue.Queue()
+    inputQ.put(root)
+
+    while not inputQ.empty():
+       
+        while not inputQ.empty():
+       
+            curr = inputQ.get()
+            print(curr.data, end=' ')
+            if curr.left!=None:
+                outputQ.put(curr.left)
+            if curr.right!=None:
+                outputQ.put(curr.right)
+       
+        print()
+        inputQ, outputQ = outputQ, inputQ
+
+
+# Main
+root = takeInput()
+k = int(stdin.readline().strip())
+rootToLeafPathsSumToK(root, k, [])
