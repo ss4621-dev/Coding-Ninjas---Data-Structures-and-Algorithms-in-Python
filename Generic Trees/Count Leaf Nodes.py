efd3e485dc3137eb35a4f3493fb08dc6d7b22ca2
SUMAN SHEKHAR <60771780+ss4621-dev@users.noreply.github.com>
2021-07-16 09:41:53 +0530

    Create Count Leaf Nodes.py

---
+++ Generic Trees/Count Leaf Nodes.py
@@ -0,0 +1,39 @@
+import sys
+class treeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+    def __str__(self):
+        return str(self.data)
+
+def leafNodeCount(tree):
+    if tree is None:
+        return 0
+    if len(tree.children) == 0:
+        return 1
+    nodeCount = 0
+    for child in tree.children:
+        nodeCount = nodeCount + leafNodeCount(child)
+    return nodeCount
+
+def createLevelWiseTree(arr):
+    root = treeNode(int(arr[0]))
+    q = [root]
+    size = len(arr)
+    i = 1
+    while i<size:
+        parent = q.pop(0)
+        childCount = int(arr[i])
+        i += 1
+        for j in range(0,childCount):
+            temp = treeNode(int(arr[i+j]))
+            parent.children.append(temp)
+            q.append(temp)
+        i += childCount
+    return root
+
+# Main
+sys.setrecursionlimit(10**6)
+arr = list(int(x) for x in input().strip().split(' '))
+tree = createLevelWiseTree(arr)
+print(leafNodeCount(tree))
