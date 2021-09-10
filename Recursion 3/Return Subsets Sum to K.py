29d99494e2a867f678c7b8985ecb922438659478
SUMAN SHEKHAR <60771780+ss4621-dev@users.noreply.github.com>
2021-09-10 10:48:25 +0530

    Create Return Subsets Sum to K.py

---
+++ Recursion 3/Return Subsets Sum to K.py
@@ -0,0 +1,22 @@
+def subsetSum(arr,k,n,op):
+    #base case
+    if len(arr)==0:
+        if k==0:
+            print(*op)
+        else:
+            pass
+        return
+    
+    newOp=[]
+    newOp=newOp+op
+    newOp.append(arr[0])
+    subsetSum(arr[1:n],k-arr[0],n,newOp)
+    subsetSum(arr[1:n],k,n,op)
+    return
+    
+    
+#main
+n=int(input())
+arr=[int(i) for i in input().split()]
+k=int(input())
+subsetSum(arr,k,n,[])
