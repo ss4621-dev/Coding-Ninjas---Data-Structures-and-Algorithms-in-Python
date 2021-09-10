ad2267c7d076aed54f87cfc4ec846cd33349b8f4
SUMAN SHEKHAR <60771780+ss4621-dev@users.noreply.github.com>
2021-09-10 10:39:49 +0530

    Create LIS.py

---
+++ DP - 1/LIS.py
@@ -0,0 +1,30 @@
+from sys import stdin
+
+def lis(li,n): 
+    dp = [[0 for j in range(2)] for i in range(n+1)]
+    
+    for i in range(n-1, -1, -1):
+        
+        including_max = 1
+        for j in range(i+1, n):
+            if li[j] > li[i]:
+                including_max = max(including_max, 1+dp[j][0])
+        dp[i][0] = including_max
+        excluding_max = dp[i+1][1]
+        overallMax = max(including_max,excluding_max)
+        dp[i][1] = overallMax
+        
+    return dp[0][1]
+      
+
+def takeInput():
+    #To take fast I/O
+    n=int(stdin.readline().strip())
+    if n==0:
+        return list(),0
+    li=list(map(int,stdin.readline().strip().split( )))
+    return li,n
+    
+
+li,n=takeInput()
+print(lis(li,n))
