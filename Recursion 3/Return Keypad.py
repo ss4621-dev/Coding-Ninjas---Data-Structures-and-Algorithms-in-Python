c3d7d4e76fd6f615ea636f84ea0dbd109e17e642
SUMAN SHEKHAR <60771780+ss4621-dev@users.noreply.github.com>
2021-09-10 10:44:32 +0530

    Create Return Keypad.py

---
+++ Recursion 3/Return Keypad.py
@@ -0,0 +1,47 @@
+def getString(d):
+    if d == 2:
+        return "abc"
+    if d == 3:
+        return "def"
+    if d == 4:
+        return "ghi"
+    if d == 5:
+        return "jkl"
+    if d == 6:
+        return "mno"
+    if d == 7:
+        return "pqrs"
+    if d == 8:
+        return "tuv"
+    if d == 9:
+        return "wxyz"
+    return " "
+
+
+
+
+def keypad(n):
+    if n == 0:
+        output = []
+        output.append("")
+        return output
+    
+    smallerNumber = n // 10
+    lastDigit = n % 10
+    
+    smallerOutput = keypad(smallerNumber)
+    optionsForLastDigit = getString(lastDigit)
+    
+    output = []
+    
+    for s in smallerOutput:
+        for c in optionsForLastDigit:
+            option = s + c
+            output.append(option)
+            
+    return output
+
+n = int(input())
+ans = keypad(n)
+for s in ans:
+    print(s)
